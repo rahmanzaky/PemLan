f55afe2c65ff6c815a19d3bb27133c5aba59f880
rahmanzaky <rahmanzaky08@gmail.com>
2024-03-03 01:54:17 +0700

    Polished

--- tugas1/LayarPenampilBuku.java
+++ tugas1/LayarPenampilBuku.java
@@ -1,7 +1,11 @@
 package tugas1;
+import java.util.Scanner;
 
 public class LayarPenampilBuku {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
         
+
     }
 }
