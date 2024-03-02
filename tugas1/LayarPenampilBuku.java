6b8ef9a45276eb3e0e319228c6ee91a1f927ea18
rahmanzaky <rahmanzaky08@gmail.com>
2024-03-03 02:53:20 +0700

    Still trying..

--- tugas1/LayarPenampilBuku.java
+++ tugas1/LayarPenampilBuku.java
@@ -4,10 +4,23 @@ import java.util.Scanner;
 public class LayarPenampilBuku {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        Perpustakaan bukuSejarah = new Perpustakaan();
+        Perpustakaan options = new Perpustakaan();
+        options.setOptions();
+
+        System.out.println("\n===============[Perpustakaan]===============");
+        for (int i = 0; i < options.getOptionsLength(); i++) {
+            System.out.printf("%d. %s%n", i + 1, options.getOptions(i));
+        }
+
+        System.out.print("\nPilihan Anda: ");
+        int userChoice = scn.nextInt();
+
+        if (userChoice == 1) {
+            
+        }
 
 
-        System.out.println(bukuSejarah.getBukuSejarah(1, 0));
+
         scn.close();
     }
 }
