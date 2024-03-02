4171f766e097d6dd8eee5ec7ec3729730327ca26
rahmanzaky <rahmanzaky08@gmail.com>
2024-03-03 02:53:47 +0700

    found a way to make it work as an object

--- tugas1/Perpustakaan.java
+++ tugas1/Perpustakaan.java
@@ -1,17 +1,39 @@
 package tugas1;
 
 public class Perpustakaan {
-    private String bukuSejarah[][] = {
-        {"Sejarah Yang Disembunyikan", "Jonathan Black", "636", "2019", "Pustaka Alfabet"},
-        {"Sejarah Dunia yang Tidak Disembunyikan", "Jonathan White", "363", "2016", "Pustaka Numerical"}
-    };
+    private String options[] = new String[9];
 
+    private String bukuTeknologi[][] = new String[6][5];
+    private String bukuFilsafat[][] = new String[6][5];
+    private String bukuSejarah[][] = new String[6][5];
+    private String bukuAgama[][] = new String[6][5];
+    private String bukuPsikologi[][] = new String[6][5];
+    private String bukuPolitik[][] = new String[6][5];
+    private String bukuFiksi[][] = new String[6][5];
 
+public void setOptions(){
+    this.options[0] = "Teknologi";
+    this.options[1] = "Filsafat";
+    this.options[2] = "Sejarah";
+    this.options[3] = "Agama";
+    this.options[4] = "Psikologi";
+    this.options[5] = "Politik";
+    this.options[6] = "Fiksi";
+    this.options[7] = "Tampilkan total buku yang tersedia";
+    this.options[8] = "Keluar dari program";
+}
 
 
 public String getBukuSejarah(int x, int y){
     return bukuSejarah[x][y];
 }
 
+
+public int getOptionsLength(){
+    return options.length;
+}
+public String getOptions(int options){
+    return this.options[options];
+}
 
 }
