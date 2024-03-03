b76fc0c0a4e615ae4802ce727d69e7f3b18e7afa
rahmanzaky <rahmanzaky08@gmail.com>
2024-03-03 19:29:19 +0700

    Finished project :)

--- tugas1/Perpustakaan.java
+++ tugas1/Perpustakaan.java
@@ -6,6 +6,7 @@ public class Perpustakaan {
         Scanner scn = new Scanner(System.in);
         PusatDataPerpustakaan dataBuku = new PusatDataPerpustakaan();
         dataBuku.setOptions();
+        boolean userContinue = false;
 
         while (true) {
             System.out.println("\n======================[Perpustakaan]=======================\n"
@@ -29,40 +30,45 @@ public class Perpustakaan {
                 continue;
             }
             
-
-            if (userChoice == 1) { // Teknologi
-                System.out.println("Buku ke");
-                for (int i = 0; i < 4; i++) {
-                    for (int j = 0; j < 4; j++) {
-                        
-                    }
+            if (userChoice >= 1 && userChoice <= 7) {
+                String[][] selectedBooks = null;
+                switch (userChoice) {
+                    case 1:
+                        selectedBooks = dataBuku.bukuTeknologi;
+                        break;
+                    case 2:
+                        selectedBooks = dataBuku.bukuFilsafat;
+                        break;
+                    case 3:
+                        selectedBooks = dataBuku.bukuSejarah;
+                        break;
+                    case 4:
+                        selectedBooks = dataBuku.bukuAgama;
+                        break;
+                    case 5:
+                        selectedBooks = dataBuku.bukuPsikologi;
+                        break;
+                    case 6:
+                        selectedBooks = dataBuku.bukuPolitik;
+                        break;
+                    case 7:
+                        selectedBooks = dataBuku.bukuFiksi;
+                        break;
                 }
 
-                
-            } else if (userChoice == 2){ // Filsafat
-
-                
-            } else if (userChoice == 3){ // Sejarah
-
-                
-            } else if (userChoice == 4){ // Agama
-
-                
-            } else if (userChoice == 5){ // Psikologi
-
-                
-            } else if (userChoice == 6){ // Politik
-
-                
-            } else if (userChoice == 7){ // Fiksi
-
-                
-            } else if (userChoice == 8){ // Total Buku
-                System.out.println("\n============================================================");
-                System.out.println("COUNT: Total buku adalah = 42");
+                int bookNumber = 1;
+                System.out.print("\n============================================================");
+                for (String[] book : selectedBooks) {
+                    System.out.println("\nBuku ke-" + bookNumber);
+                    System.out.println("1.  Nama Buku   \t: " + book[0]);
+                    System.out.println("2.  Penulis     \t: " + book[1]);
+                    System.out.println("3.  Jumlah Halaman\t: " + book[2]);
+                    System.out.println("4.  Tahun Terbit\t: " + book[3]);
+                    System.out.println("5.  Nama Penerbit\t: " + book[4]);
+                    bookNumber++;
+                }
                 System.out.println("============================================================");
 
-                boolean userContinue = false;
                 do {
                     System.out.println("Apakah anda ingin melanjutkan program? (ya/tidak)");
                     System.out.print("Pilihan Anda: ");
@@ -85,23 +91,46 @@ public class Perpustakaan {
                         userContinue = true;
                     }   
                 } while (userContinue);
-
+            } else if (userChoice == 8) {
+                System.out.println("\n============================================================");
+                System.out.println("COUNT: Total buku adalah = 42");
+                System.out.println("============================================================");
 
                 
-            } else if (userChoice == 9){ // Keluar dari Program
+                do {
+                    System.out.println("Apakah anda ingin melanjutkan program? (ya/tidak)");
+                    System.out.print("Pilihan Anda: ");
+                    String userChoiceString = scn.nextLine();
+
+                    if (userChoiceString.equalsIgnoreCase("ya")) {
+                        userContinue = false;
+                        break;
+                    } else if(userChoiceString.equalsIgnoreCase("tidak")) {
+                        userContinue = false;
+                        System.out.println("\n============================================================");
+                        System.out.println("Terima kasih telah menggunakan program ini.");
+                        System.out.println("Sampai jumpa di lain kesempatan!");
+                        System.out.println("============================================================");
+                        System.exit(0);
+                    } else {
+                        System.out.println("\n============================================================");
+                        System.out.println("Pilihan tidak valid. Silakan ketik 'ya' atau 'tidak'.");
+                        System.out.println("============================================================");
+                        userContinue = true;
+                    }   
+                } while (userContinue);
+            } else if (userChoice == 9) {
                 break;
             } else {
                 System.out.println("\n============================================================");
                 System.out.println("Pilihan tidak valid. Silakan pilih angka 1-9.");
                 System.out.println("============================================================");
             }
-
         }
 
         System.out.println("\n============================================================");
         System.out.println("Terima kasih telah menggunakan program ini.");
         System.out.println("Sampai jumpa di lain kesempatan!");
         System.out.println("============================================================");
-        
     }
 }
